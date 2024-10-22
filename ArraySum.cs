55e788f95be818be2a04e90c60fb79dd7acbb690
Color-Kat <katik7979@gmail.com>
2024-10-22 09:04:57 +0300

    Добавьте файлы проекта.

---
+++ ArraySum.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArraySum
+{
+    internal class ArraySum
+    {
+
+        private float[] array;
+        public float result;
+
+        DLLWrapper dllSum = new DLLWrapper();
+
+        public void GenerateRandomArray(int length)
+        {
+            array = new float[length];
+
+            Random random = new Random();
+
+            for(var i = 0; i < array.LongLength; i++) {
+                array[i] = (float) random.Next(0, 10);
+            }
+        }
+
+        public string CSharpSum()
+        {
+            if (array == null) return @"Array is not generated";
+
+            Stopwatch timer = new Stopwatch();
+            timer.Start();
+
+            result = 0;
+
+            for (var i = 0; i < array.LongLength; i++)
+            {
+                result = result + array[i];
+            }
+
+            timer.Stop();
+
+            return string.Format("Sum: {0}\nTime: {1}", result, timer.Elapsed);
+        } 
+
+        public string CppSum()
+        {
+            if (array == null) return @"Array is not generated";
+
+            Stopwatch timer = new Stopwatch();
+            timer.Start();
+
+            result = dllSum.SumCpp(array);
+
+            timer.Stop();
+
+            return string.Format("Sum: {0}\nTime: {1}", result, timer.Elapsed);
+        }
+
+        public string XMMSum()
+        {
+            if (array == null) return @"Array is not generated";
+
+            Stopwatch timer = new Stopwatch();
+            timer.Start();
+
+            result = dllSum.SumXMM(array);
+            timer.Stop();
+
+            return string.Format("Sum: {0}\nTime: {1}", result, timer.Elapsed);
+        }
+    }
+}
---
+++ DLLWrapper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArraySum
+{
+    internal class DLLWrapper
+    {
+
+        public float SumCpp(float[] array)
+        {
+            return DLLSum(array, array.Length);
+        }
+
+        [DllImport("D:/Programming/C#/MyDLL/ArraySum/Debug/CppSum.dll", EntryPoint = "ArraySum", CharSet = CharSet.Auto, SetLastError = false)]
+        private static extern float DLLSum(float[] array, int length);
+
+       public float SumXMM(float[] array)
+        {
+            return DLLSumXMM(array, array.Length);
+        }
+
+        [DllImport("D:/Programming/C#/MyDLL/ArraySum/Debug/CppSum.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "SumXMM", CharSet = CharSet.Auto, SetLastError = false)]
+        private static extern float DLLSumXMM(float[] array, int length);
+    }
+}
